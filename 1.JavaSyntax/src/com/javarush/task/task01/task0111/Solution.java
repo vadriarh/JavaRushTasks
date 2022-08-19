54cf995999a6cc9a5c82fb56da87bd17ee1e4598
Vadim Sabodakho <vadriarh@gmail.com>
2022-08-19 11:22:36 +0200

    initialize source Syntax level 1

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0111/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0111;
+
+/* 
+Одной переменной недостаточно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a=5;
+        int b=23;
+        String s="name";
+    }
+}
